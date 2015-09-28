d89b48916f4cba87dbbe6a4aff94ec82832bfa4c
Helder Martins <heldergaray@gmail.com>
2015-09-28 23:06:15 +0200

    Added database sample query

--- shalk/shalk.py
+++ shalk/shalk.py
@@ -2,15 +2,30 @@
 import tornado.ioloop
 from tornado.web import Application, RequestHandler
 from tornado.options import define, options
-
+import os
+import pymongo as pym
 
 
 class NgramsHandler(RequestHandler):
+
+    def initialize(self, db):
+        self.db = db
+
     def get(self):
+        # optional query string arg
         freq = self.get_argument('freq', None)
+
+        # required query string arg
         word0 = self.get_argument('word0')
 
-        self.write('freq = {0}, word0 = {1}'.format(freq, word0))
+        query = { 'word0': word0 }
+        if freq:
+            query['freq'] = freq
+
+        # query the database
+        ngram = self.db['ngrams'].find_one(query)
+
+        self.write('ngrams = {0}'.format(ngram))
 
 
 def main():
@@ -18,7 +33,11 @@ def main():
     define("port", default=8080, help="Port")
     tornado.options.parse_command_line()
 
-    application = Application([(r"/ngrams/find", NgramsHandler),])
+    # init database connection
+    client = pym.MongoClient(os.getenv('OPENSHIFT_MONGODB_DB_URL'))
+    db = client['shalk']
+
+    application = Application([(r"/ngrams/find", NgramsHandler, dict(db=db)),])
 
     application.listen(options.port, options.host)
     tornado.ioloop.IOLoop.instance().start()
