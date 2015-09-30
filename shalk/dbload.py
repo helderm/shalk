ea66d9195036caecea44ed9c93d86f341531b1cf
Helder Martins <heldergaray@gmail.com>
2015-09-30 21:35:57 +0200

    Modified db load script

--- shalk/dbload.py
+++ shalk/dbload.py
@@ -40,8 +40,6 @@ def load_file_into_db(db, datafile):
 
     print '* Importing file [{0}] into db...'.format(datafile)
 
-    count = 0
-    mod = 20000
     with open(datafile, 'r') as f:
         ngrams = []
         for line in f:
@@ -56,17 +54,9 @@ def load_file_into_db(db, datafile):
 
             ngrams.append(ngram)
 
-            count += 1
-            if count % mod == 0:
-                print '- Inserting [{0}] ngrams into db...'.format(len(ngrams))
-                sys.stdout.flush()
-
-                db['ngrams'].insert_many(ngrams)
-                ngrams = []
-
-            #ngrams.append(ngram)
-
     print '- Inserting [{0}] ngrams into db...'.format(len(ngrams))
+    sys.stdout.flush()
+
     db['ngrams'].insert_many(ngrams)
 
     print '* Finished importing file [{0}]!'.format(datafile)
