d9512ee5238edc8639fa0d0e764519ef024b8adb
Marcel Binz <marcelbinz1@gmail.com>
2015-10-12 15:52:22 +0200

    added grammar.

--- shalk/PoemTemplate.py
+++ shalk/PoemTemplate.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pickle
+import scipy.stats as stats
+import random
 
 
 class PoemTemplate:
@@ -14,10 +17,15 @@ class PoemTemplate:
     standardDerivation = 1;
 
     def __init__(self, mP, rS):
+        self.grammar = pickle.load(open("grammar", "rb"))
+        self.grammarDistribution = [len(i) for i in self.grammar]
         self.metricPattern = mP
         self.rhymeScheme = rS
 
+
     def createTemplate(self):
+
+        ## generate the metric template
         temp = []
         # for each line
         for s in self.metricPattern:
@@ -32,7 +40,54 @@ class PoemTemplate:
                 lineLength -= syllables
 
             temp.append(line)
-        return temp
+
+        ## generate grammar tempate
+        word_count = 0
+        for l in temp:
+            for w in l:
+                word_count = word_count + 1
+
+        print(word_count)
+        all_sents = []
+        all_len = []
+        while word_count > 0:
+
+            partition = sum(self.grammarDistribution)
+            normalized_distribution = [float(x) / partition for x in self.grammarDistribution]
+
+            dis = stats.rv_discrete(name='dis', values=(range(len(self.grammarDistribution)), normalized_distribution))
+            sentence_length = dis.rvs(size=1)
+            all_len.append(sentence_length[0])
+
+            word_count = word_count - sentence_length
+
+        if word_count < 0:
+            for i in range(abs(word_count)):
+                possible_choices = np.where(np.asarray(all_len) > 2)[0]
+                choice = random.choice(possible_choices)
+                all_len[choice] = all_len[choice] - 1
+
+        print(all_len)
+
+        for i in range(len(all_len)):
+           all_sents.append(random.choice(self.grammar[all_len[i]]))
+        print(all_sents)
+
+        all_sents = [i for sl in all_sents for i in sl]
+
+        i = 0
+        final_template = []
+        for j, l in enumerate(temp):
+            final_template.append([])
+            for w in l:
+                if all_sents[i] == ".":
+                    i = i+1
+                final_template[j].append([w, all_sents[i]])
+                i = i + 1
+
+
+        print(final_template)
+        return final_template
 
 
 
