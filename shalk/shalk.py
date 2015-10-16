a39e7d8967f92d01564abc6dddb10f92546551da
Helder Martins <heldergaray@gmail.com>
2015-10-16 11:15:34 +0200

    Removed args for sonnets

--- shalk/shalk.py
+++ shalk/shalk.py
@@ -5,7 +5,7 @@ from poem import Poem
 def main():
 
     parser = argparse.ArgumentParser(prog='Shalk', description='The best poem generator ever!')
-    parser.add_argument('-t', '--type', help='Poem type', choices=['haiku', 'tanka', 'limerick', 'quatrain', 'spens_sonnet', 'ital_sonnet', 'shaks_sonnet'], default='haiku') #TODO: add others
+    parser.add_argument('-t', '--type', help='Poem type', choices=['haiku', 'tanka', 'limerick', 'quatrain'], default='haiku') #TODO: add others
     parser.add_argument('-n', '--quantity', help='Quantity of poems generated', default=10)
     parser.add_argument('-s', '--smoothing', help='Smoothing algorithm', choices=['linear', 'backoff'], default='backoff')
     args = parser.parse_args()
